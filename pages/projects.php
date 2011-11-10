b1557c685ae6ff9d47b953b14072371db62b8115
Ruben de Vries <rubensayshi@gmail.com>
2011-11-10 22:34:42 +0100

    added voiture and nodeJS

--- pages/projects.php
+++ pages/projects.php
@@ -1,1 +1,51 @@
-<p>projects should go here</p>
+<p>
+	I really love doing side projects, they often allow for a lot of freedom! <br />
+	For a list of projects I've done for clients you should check out the <a href="<?php echo url("portfolio"); ?>">portfolio</a> page, this page only lists my personal projects.<br />
+	For a lot of projects you can only find some code on github, if you want to see the projects in action I'd be happy to show you since most are running on my laptop! <br />
+</p>
+
+<h4 class="long">Voiture</h4>
+<ul>
+	<li><a href="http://voiture.hoppinger.com">Voiture documentation website</a></li>
+</ul>
+<p>
+	Drupal is strongly build and focused towards being able to stack a lot of contrib modules on top of each other, all managed through a web interface. <br />
+	All the configuration is stored in the database and mixed in with the content, as a result it becomes very hard to capture the configuration (which contains / powers a lot of functionality) in version control. <br />
+	To be able do automated deployment for a Drupal project, working on a local environment, deploying test release, live release, hotfixes and such we either needed a can of cheap students to do everything by hand withouth any errors or <strong>voiture</strong>! <br />
+	<br />
+	We've created tools to split the configuration from the database, place it in version control and push it back into the database on other environments!<br />
+	Checkout the documentation website for a lot more information! <br />
+	<br />
+	Maybe our fight against Drupal was hard and made my brain hurt sometime, but in the end the result was well worth it and we got to do some presentations about it in the process! <br />
+	
+	<ul>
+		<li>Drupal</li>
+		<li>Automated deployment</li>
+		<li>Community</li>
+		<li>Presentation</li>
+	</ul>
+</p>
+
+<h4 class="long">NodeJS</h4>
+<p>
+	I love javascript and I love NodeJS! <br />
+	If you check out my github account you'll notice a lot of small NodeJS projects and some forks and watches of other NodeJS projects. <br />
+	Unfortunatly I haven't gotten a chance to use it for a real project and get a webapplication in production using NodeJS yet, but I hope someday I'll get a chance! <br />
+</p>
+<h5>Timerime reloaded</h5>
+<ul>
+	<li><a href="https://github.com/rubensayshi/node-timerime-reloaded">Project on github</a></li>
+</ul>
+<p>
+	During my time at Hoppinger I've done a lot of maintanance on the <a href="http://www.timerime.com">timerime</a> website. <br />
+	Unfortunatly some bad choices in the early development and a lot more growth then anticipated cause this website to run into a lot performance issues! <br />
+	<br />
+	Combining that with my ever lasting search of a real project which I could do with NodeJS, I've created a small prototype called 'timerime reloaded' with NodeJS and MongoDB. <br />
+</p>
+<h5>MyBench</h5>
+<ul>
+	<li><a href="https://github.com/rubensayshi/mybench">Project on github</a></li>
+</ul>
+<p>
+	So during my quest to find a real project for NodeJS I wanted to benchmark some pieces of code and to do so I created a small tool which can startup a NodeJS application and run apache benchmark against it! <br />
+</p>
