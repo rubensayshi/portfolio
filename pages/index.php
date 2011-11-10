aaed9be0d3c87a5c73446b638c683148740c312d
Ruben de Vries <rubensayshi@gmail.com>
2011-11-10 22:58:59 +0100

    using url() for internal urls

--- pages/index.php
+++ pages/index.php
@@ -13,18 +13,18 @@
 	Developing so-called 'webapplications' requires a continiously expanding skillset to fit the needs of clients and an open mind to any new techniques out there. <br />
 	Actually, any webdeveloper who really loves his job goes being an 'open mind' and is always searching for the next new thing which might change the world!
 </p>
-<a class="more" href="/aboutme"><h5>More about me</h5></a>
+<a class="more" href="<?php echo url("aboutme"); ?>"><h5>More about me</h5></a>
 
 <h4>This Website</h4>
 <p>
 	I've created this website to tell the world a little bit about myself. <br />
 	And ofcourse because I love building websites and I'm always looking for a reason to create a new website and try out new things! <br />
 </p>
-<a class="more" href="/aboutsite"><h5>More about this website</h5></a>
+<a class="more" href="<?php echo url("aboutsite"); ?>"><h5>More about this website</h5></a>
 
 <h4>Highlighted project</h4>
 <p>
 	<strong>Voiture!</strong>
 	Voiture is an SVN-based integrated deployment automation tool for small development teams that develop Drupal (v6 or v7) sites on an *nix/LAMP stack!
 </p>
-<a class="more" href="/aboutme"><h5>More projects</h5></a>
+<a class="more" href="<?php echo url("projects"); ?>"><h5>More projects</h5></a>
