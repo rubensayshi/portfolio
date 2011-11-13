60b327be09f092d2b78d3263329562e54d189f68
Ruben de Vries <rubensayshi@gmail.com>
2011-11-13 12:53:38 +0100

    copy paste of word doc

--- pages/resume.php
+++ pages/resume.php
@@ -1,1 +1,103 @@
-<p>resume should go here</p>
+<h2>Curriculum Vitea</h2>
+<h4>Personalia</h4>
+<table>
+	<tr>
+		<th>Naam:</th>
+		<td>Ruben de Vries</td>
+	</tr>
+	<tr>
+		<th>Adres:</th>
+		<td>Lavendel  13 <br />
+			2951DA Alblasserdam
+		</td>
+	</tr>
+	<tr>
+		<th>Telefoon:</th>
+		<td>06 12227277</td>
+	</tr>
+	<tr>
+		<th>E-mail:</th>
+		<td>ruben@rubensayshi.com</td>
+	</tr>
+	<tr>
+		<th>Geboortedatum:</th>
+		<td>6 augustus 1987</td>
+	</tr>
+	<tr>
+		<th>Geboorteplaats:</th>
+		<td>Alblasserdam</td>
+	</tr>
+	<tr>
+		<th>Nationaliteit:</th>
+		<td>Nederlander</td>
+	</tr>
+	<tr>
+		<th>Sofi-nummer:</th>
+		<td>125588677</td>
+	</tr>
+</table>
+
+<h4>Opleidingen</h4>
+<table>
+	<tr>
+		<td>1999-2004</td>	<th>HAVO Willem de Zwijger Collega</th>	<td>Papendrecht</td>
+	</tr>
+	<tr>
+		<td>2004-2008</td>	<th>Hogeschool Rotterdam – Grafimedia Technologie</th>	<td>Rotterdam</td>
+	</tr>
+</table>
+
+<h4>Werkervaring</h4>
+<table>
+	<tr>
+		<th>Restaurant de Poort</th>	<td>Februari 2003 - Juli 2008</td>
+	</tr>
+	<tr>
+		<th>Functie:</th>
+		<td>Kok</td>
+	</tr>
+	
+Adres:</th>
+		<td>Eind 19
+                4201CP Gorinchem
+
+Hoppinger</th>
+		<td>Augustus 2008 - December 2011
+Functie:</th>
+		<td>Webdeveloper
+Adres:</th>
+		<td>Lloydstraat 138c
+                3024EA Rotterdam
+Telefoon:</th>
+		<td>010 2210190
+Referentie:</th>
+		<td>Martijn Dragt
+                Jochem Maas
+
+Talen
+Nederlands            Moedertaal
+Engels            Gesproken - Redelijk, Geschreven - Goed
+Kennis binnen vakgebied
+
+Serverside:    PHP, NodeJS
+MySQL (SQLLite, MSSQL), MongoDB
+Linux, Apache2, Varnish, Apache SOLR
+Bash, Ruby, Lingo, XML
+
+Frontend:    (X)HTML, HTML5
+CSS, CSS3
+LESS
+Javascript, AJAX
+jQuery, MooTools, Prototype
+Flash
+
+Version Control:    CVS, SVN, GIT (gitflow)
+
+Frameworks:    Symfony2
+ExpressJS
+
+CMS:    Drupal
+Joomla
+Wordpress
+Automation:            Voiture (Drupal)
+                Capistrano
