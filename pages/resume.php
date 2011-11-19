9ec10c01bcd0afe36690b535b5a8c37637ac16ca
Ruben de Vries <rubensayshi@gmail.com>
2011-11-19 20:10:10 +0100

    (re)did resume

--- pages/resume.php
+++ pages/resume.php
@@ -1,103 +1,145 @@
-<h2>Curriculum Vitea</h2>
+<h2 class="long">Curriculum Vitea</h2>
 <h4>Personalia</h4>
-<table>
-	<tr>
-		<th>Naam:</th>
-		<td>Ruben de Vries</td>
-	</tr>
-	<tr>
-		<th>Adres:</th>
-		<td>Lavendel  13 <br />
-			2951DA Alblasserdam
-		</td>
-	</tr>
-	<tr>
-		<th>Telefoon:</th>
-		<td>06 12227277</td>
-	</tr>
-	<tr>
-		<th>E-mail:</th>
-		<td>ruben@rubensayshi.com</td>
-	</tr>
-	<tr>
-		<th>Geboortedatum:</th>
-		<td>6 augustus 1987</td>
-	</tr>
-	<tr>
-		<th>Geboorteplaats:</th>
-		<td>Alblasserdam</td>
-	</tr>
-	<tr>
-		<th>Nationaliteit:</th>
-		<td>Nederlander</td>
-	</tr>
-	<tr>
-		<th>Sofi-nummer:</th>
-		<td>125588677</td>
-	</tr>
-</table>
+<div class="row">
+	<div class="span2">Naam:</div>
+	<div class="span3">Ruben de Vries</div>
+</div>
+<div class="row">
+	<div class="span2">Adres:</div>				
+	<div class="span3">Lavendel  13 <br />
+					  2951DA Alblasserdam
+	</div>
+</div>
+<div class="row">
+	<div class="span2">Telefoon:</div>
+	<div class="span3">06 12227277</div>
+</div>
+<div class="row">	
+	<div class="span2">E-mail:</div>
+	<div class="span3">ruben@rubensayshi.com</div>
+</div>
+<div class="row">
+	<div class="span2">Geboortedatum:</div>
+	<div class="span3">6 augustus 1987</div>
+</div>
+<div class="row">
+	<div class="span2">Geboorteplaats:</div>
+	<div class="span3">Alblasserdam</div>
+</div>
+<div class="row">
+	<div class="span2">Nationaliteit:</div>
+	<div class="span3">Nederlander</div>
+</div>
+
+<div class="spacer"></div>
 
 <h4>Opleidingen</h4>
-<table>
-	<tr>
-		<td>1999-2004</td>	<th>HAVO Willem de Zwijger Collega</th>	<td>Papendrecht</td>
-	</tr>
-	<tr>
-		<td>2004-2008</td>	<th>Hogeschool Rotterdam – Grafimedia Technologie</th>	<td>Rotterdam</td>
-	</tr>
-</table>
+<div class="row">
+	<div class="span3">HAVO - Willem de Zwijger College</div>				
+	<div class="span1">1999-2004</div>	
+	<div class="span1">Papendrecht</div>
+</div>
+<div class="row">
+	<div class="span3">Grafimedia Technologie - Hogeschool Rotterdam</div>
+	<div class="span1">2004-2008</div>
+	<div class="span1">Rotterdam</div>
+</div>
+
+<div class="spacer"></div>
 
 <h4>Werkervaring</h4>
-<table>
-	<tr>
-		<th>Restaurant de Poort</th>	<td>Februari 2003 - Juli 2008</td>
-	</tr>
-	<tr>
-		<th>Functie:</th>
-		<td>Kok</td>
-	</tr>
-	
-Adres:</th>
-		<td>Eind 19
-                4201CP Gorinchem
+<h5>Restaurant de Poort</h5>
+<div class="row">
+	<div class="span2">Periode:</div>
+	<div class="span3">Februari 2003 - Juli 2008</div>
+</div>
+<div class="row">
+	<div class="span2">Functie:</div>
+	<div class="span3">Kok</div>
+</div>
+<div class="row">
+	<div class="span2">Adres:</div>
+	<div class="span3">Eind 19
+						4201CP Gorinchem
+	</div>
+</div>
 
-Hoppinger</th>
-		<td>Augustus 2008 - December 2011
-Functie:</th>
-		<td>Webdeveloper
-Adres:</th>
-		<td>Lloydstraat 138c
-                3024EA Rotterdam
-Telefoon:</th>
-		<td>010 2210190
-Referentie:</th>
-		<td>Martijn Dragt
-                Jochem Maas
-
-Talen
-Nederlands            Moedertaal
-Engels            Gesproken - Redelijk, Geschreven - Goed
-Kennis binnen vakgebied
+<div class="spacer"></div>
 
-Serverside:    PHP, NodeJS
-MySQL (SQLLite, MSSQL), MongoDB
-Linux, Apache2, Varnish, Apache SOLR
-Bash, Ruby, Lingo, XML
+<h5>Hoppinger</h5>
+<div class="row">
+	<div class="span2">Periode:</div>
+	<div class="span3">Augustus 2008 - December 2011</div>
+</div>
+<div class="row">
+	<div class="span2">Functie:</div>
+	<div class="span3">Webdeveloper</div>
+</div>
+<div class="row">
+	<div class="span2">Adres:</div>
+	<div class="span3">Lloydstraat 138c
+</div>
+<div class="row">
+	<div class="span2">Telefoon:</div>
+	<div class="span3">010 2210190</div>
+</div>
+<div class="row">
+	<div class="span2">Referentie:</div>
+	<div class="span3">Martijn Dragt<br />
+						Jochem Maas
+	</div>
+</div>
 
-Frontend:    (X)HTML, HTML5
-CSS, CSS3
-LESS
-Javascript, AJAX
-jQuery, MooTools, Prototype
-Flash
+<div class="spacer"></div>
 
-Version Control:    CVS, SVN, GIT (gitflow)
+<h4>Talen</h4>
+<div class="row">
+	<div class="span2">Nederlands</div>
+	<div class="span3">Moedertaal</div>
+</div>
+<div class="row">
+	<div class="span2">Engels</div>
+	<div class="span3">Gesproken - Redelijk <br />
+    					Geschreven - Goed
+    </div>
+</div>
 
-Frameworks:    Symfony2
-ExpressJS
+<div class="spacer"></div>
 
-CMS:    Drupal
-Joomla
-Wordpress
-Automation:            Voiture (Drupal)
-                Capistrano
+<h4>Kennis binnen vakgebied</h4>
+<div class="row">
+	<div class="span2">Serverside:</div>
+	<div class="span3">PHP, NodeJS<br />
+						MySQL (SQLLite, MSSQL), MongoDB<br />
+						Linux, Apache2, Varnish, Apache SOLR<br />
+						Bash, Ruby, Lingo, XML
+	</div>
+</div>
+<div class="row">
+	<div class="span2">Frontside:</div>
+	<div class="span3">(X)HTML, HTML5<br />
+						CSS, CSS3<br />
+						LESS<br />
+						Javascript, AJAX<br />
+						jQuery, MooTools, Prototype<br />
+						Flash
+	</div>
+</div>
+<div class="row">
+	<div class="span2">Frameworks:</div>
+	<div class="span3">Symfony2<br />
+						ExpressJS
+	</div>
+</div>
+<div class="row">
+	<div class="span2">CMS:</div>
+	<div class="span3">Drupal<br />
+						Joomla<br />
+						Wordpress
+	</div>
+</div>
+<div class="row">
+	<div class="span2">Deployment automation:</div>
+	<div class="span3">Voiture (Drupal)<br />
+						Capistrano</div>
+</div>
