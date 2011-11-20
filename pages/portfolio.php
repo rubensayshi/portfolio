f4f4f4d56b039257e35fa00a0ee0274372472372
Ruben de Vries <rubensayshi@gmail.com>
2011-11-20 20:20:50 +0100

    matrix page with random images

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -1,11 +1,57 @@
+<?php 
+
+$rows = 5;
+$cols = 4;
+
+function build_matrix_col_classes($r, $c, $rows, $cols)
+{
+	$classes = array();
+	
+	$classes[] = "span";
+	$classes[] = "col{$c}";
+	$classes[] = ($c%2 == 0 ? 'even' : 'odd');
+
+	if ($c == $cols) {
+		$classes[] = 'last';
+	}
+	
+	if ($c == 1) {
+		$classes[] = 'first';
+	}
+	
+	return implode(" ", $classes);
+}
+
+$webroot	= dirname(dirname(__FILE__)) . '/httpdocs/';
+$imagedir	= dirname(dirname(__FILE__)) . '/httpdocs/images/portfolio'; 
+$images		= glob("{$imagedir}/*/*_small.png");
+
+function random_image($images, $webroot) 
+{
+	$k = array_rand($images);
+	$image = $images[$k];
+	
+	unset($images[$k]);
+	
+	return str_replace($webroot, '', $image);
+}
+
+
+?>
+
 <h1>Portfolio</h1>
 
 <div class="matrix">
-	<div class="row">
-		<?php for ($i = 1; $i <= 3; $i++): ?>
-			<div class="col<?php echo $i; ?>">
-				<img src="<?php echo asset('images/portfolio/'); ?>" />
-			</div>
-		<?php endfor; ?>
-	</div>
+	<?php for ($r = 1; $r <= $rows; $r++): ?>
+		<div class="row <?php echo ($r%2 == 0 ? 'even' : 'odd'); ?>">
+			<?php for ($c = 1; $c <= $cols; $c++): ?>
+				<div class="<?php echo build_matrix_col_classes($r, $c, $rows, $cols); ?>">
+					<img src="<?php echo asset(random_image($images, $webroot)); ?>" />
+					<div class="mask mask-1"></div>
+					<div class="mask mask-2"></div>
+					<div class="description">Project #<?php echo ($r * $c); ?></div>
+				</div>
+			<?php endfor; ?>
+		</div>
+	<?php endfor; ?>
 </div>
