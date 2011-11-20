e646ce3ebb8a4dfd894309272fd9ca395dc77121
Ruben de Vries <rubensayshi@gmail.com>
2011-11-20 21:21:41 +0100

    added template for gites

---
+++ templates/portfolio/gites.tpl.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * @var	array	$project			the project to display
+ */
+?>
+<p>
+	Gites is a very special relation for Hoppinger since they share the same owners (sort of). <br />
+	It's a webapplication which allows house owners to advertise for rental or sale of their vacation houses in france. <br />
+	The previous website had become rather old and maintaining it (for which I was the main developer) became more and more problematic, let alone developing new features for it! <br />
+	<br />
+	For the new website (version 5!) we started from scratch and rebuild all the old features, ofcourse not before making new interaction and graphical designs. <br />
+	Over time the website has grown so much and the amount of functionality we had te reimplement was huge! <br />
+
+	The website contains a frontend for visitors, a backend for the owners of the houses, a backend / admin system for the administrators of Gites and a automated invoicing chain. <br />
+	<br />
+	During the start of the project all the developers at Hoppinger were still very 'not invented here' orientated and all projects were build on top of the HoppingerCMS, so was Gites. <br />
+	The HoppingerCMS was build to create business websites of small to medium scale and be very flexible to match the needs of the clients. <br />
+	It however was not build for a high load website and for Gites we rewrote a lot of the code on the CMS! <br />
+	<ul>
+		<li>HoppingerCMS</li>
+
+		<li>Invoicing</li>
+		<li>Lead developer</li>
+	</ul>
+</p>
