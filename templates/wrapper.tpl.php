17658c712dec697d9688ddd2f1f08108b4e0f844
Ruben de Vries <rubensayshi@gmail.com>
2011-11-21 19:57:11 +0100

    HTML5 tags in IE, part 1

--- templates/wrapper.tpl.php
+++ templates/wrapper.tpl.php
@@ -15,6 +15,22 @@
 	<link rel="stylesheet" href="<?php echo asset('css/style.css'); ?>" />
 	
 	<title><?php echo ($page['page'] != 'index' ? "{$page['title']} | " : '') ?>Ruben de Vries</title>
+	
+	<!--[if IE]>
+	<script type="text/javascript">
+		(function() {
+			var html5elmeents = "address|article|aside|audio|
+			canvas|command|datalist|details|dialog|
+			figure|figcaption|footer|header|hgroup|
+			keygen|mark|meter|menu|nav|progress|
+			ruby|section|time|video".split('|');
+			
+			for(var i = 0; i < html5elmeents.length; i++){
+				document.createElement(html5elmeents[i]);
+			}
+		})();
+	</script>
+	<![endif]-->
 </head>
 <body>
 <div id="container" class="container_12">
