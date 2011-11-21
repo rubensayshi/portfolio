aad0e7bc2f63da342f67075187c967c005ea5b21
Ruben de Vries <rubensayshi@gmail.com>
2011-11-21 17:35:22 +0100

    greater than sign looks better

--- templates/project.tpl.php
+++ templates/project.tpl.php
@@ -15,7 +15,7 @@ add_javascript_script(asset('js/carousel.js'));
 <?php if ($project['tags']): ?>
 <ul class="tags">
 	<?php foreach ($project['tags'] as $tag): ?>
-		<li><small>&lt;</small> <?php echo $tag; ?></li>
+		<li><small>&gt;</small> <?php echo $tag; ?></li>
 	<?php endforeach; ?>
 </ul>
 <?php endif; ?>
