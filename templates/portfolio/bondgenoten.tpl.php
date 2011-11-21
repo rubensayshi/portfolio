f82b9571428f149b04bd0e53a1fb60a7b3e2157d
Ruben de Vries <rubensayshi@gmail.com>
2011-11-21 17:34:52 +0100

    added text content to all portolio projects

---
+++ templates/portfolio/bondgenoten.tpl.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * @var	array	$project			the project to display
+ */
+?>
+<h3>About this project</h3>
+<p>
+	Bondgenoten is a combination of 3 (web / interaction)design bureaus. <br />
+	We developed a webapplication which produces a seperate website for 3 bureaus, but also combines their portfolio and news on their combined bondgenoten website. <br /> 
+</p>
+<p>
+	For me it was a lot of fun building this website since the code had to be very transparent and seperated to offer the same functionality on 3 different sites and combine it of the main 
+</p>
---
+++ templates/portfolio/qhome.tpl.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @var	array	$project			the project to display
+ */
+?>
+<h3>About this project</h3>
+<p>
+	Qhome offers vacation homes for rent (and sale) in France. <br />
+	Originally a client of <a href="<?php echo url("portfolio/gites"); ?>">Gites</a> they wanted a seperate website with their own brand and more information about their company. <br />
+	Since they already supplied Gites with a lot of content about the vacation homes we've created a sync from the Gites website to their website over a simple API. 
+</p>
+<p>
+	Creating syncs between websites can always be tedious but in return the complexity is also a lot of fun!
+</p>
