2a975e37851ff8dc5abde0d3a3d9b3c8be08f862
Ruben de Vries <rubensayshi@gmail.com>
2011-12-01 11:48:17 +0100

    better print-friendly pdf

--- bin/resumepdf.sh
+++ bin/resumepdf.sh
@@ -11,11 +11,11 @@ URL="http://ruben.portfolio.nl/resume"
 DEST="${ROOT}/httpdocs/pdf"
 
 NORMAL_PDF="${DEST}/cv.pdf"
-wkhtmltopdf --no-background --margin-bottom 0mm --margin-top 0mm --margin-left 0mm --margin-right 0mm ${URL}?pdf=1 ${NORMAL_PDF}
+wkhtmltopdf --background --margin-bottom 0mm --margin-top 0mm --margin-left 0mm --margin-right 0mm ${URL}?pdf=1 ${NORMAL_PDF}
 ${SUDO_DO} chmod 0770 ${NORMAL_PDF}
 ${SUDO_DO} chown $DEV_USR:$APACHE_USR_GRP ${NORMAL_PDF}
 
 PRINT_PDF="${DEST}/cv_print.pdf"
-wkhtmltopdf --no-background --margin-bottom 5mm --margin-top 5mm --margin-left 5mm --margin-right 5mm ${URL}?printpdf=1 ${PRINT_PDF}
+wkhtmltopdf --background --margin-bottom 5mm --margin-top 5mm --margin-left 5mm --margin-right 5mm ${URL}?printpdf=1 ${PRINT_PDF}
 ${SUDO_DO} chmod 0770 ${PRINT_PDF}
 ${SUDO_DO} chown $DEV_USR:$APACHE_USR_GRP ${PRINT_PDF}
